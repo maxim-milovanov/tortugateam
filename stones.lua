d35ef56c7e4ef81122f4c81b5e0026b18481ed04
Maxim <mg.milovanov@ya.ru>
2023-03-25 13:47:06 +0300

    Create stones.lua

---
+++ stones.lua
@@ -0,0 +1,271 @@
+STONE = {
+	-- 
+	[0] = {item_id = 885, attr_type = 0, max_lv = 4, gear_point = 0, eff = 0, chance_combine = {1, 1, 1, 0.9, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 1}},
+	
+	-- Fiery Gem 
+	[1] = {item_id = 878, attr_type = ITEMATTR_VAL_MNATK, max_lv = 4, gear_point = 1, eff = 10, chance_combine = {1, 1, 0.8, 0.7, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.8, 0.7, 0, 0, 0, 0, 0}},
+ 
+	-- Furious Gem 
+	[2] = {item_id = 879, attr_type = ITEMATTR_VAL_MNATK, max_lv = 4, gear_point = 1, eff = 12, chance_combine = {1, 1, 0.8, 0.7, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.8, 0.7, 0, 0, 0, 0, 0}},
+ 
+	-- Explosive Gem 
+	[3] = {item_id = 880, attr_type = ITEMATTR_VAL_MNATK, max_lv = 4, gear_point = 1, eff = 15, chance_combine = {1, 1, 0.8, 0.7, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.8, 0.7, 0, 0, 0, 0, 0}},
+ 
+	-- Lustrious Gem 
+	[4] = {item_id = 881, attr_type = ITEMATTR_VAL_HIT, max_lv = 3, gear_point = 2, eff = 5, chance_combine = {1, 1, 0.8, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.8, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Glowing Gem 
+	[5] = {item_id = 882, attr_type = ITEMATTR_VAL_DEF, max_lv = 4, gear_point = 1, eff = 7, chance_combine = {1, 1, 0.8, 0.7, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.8, 0.7, 0, 0, 0, 0, 0}},
+ 
+	-- Shining Gem 
+	[6] = {item_id = 883, attr_type = ITEMATTR_VAL_MXHP, max_lv = 4, gear_point = 2, eff = 120, chance_combine = {1, 1, 0.8, 0.7, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.8, 0.7, 0, 0, 0, 0, 0}},
+ 
+	-- Shadow Gem 
+	[7] = {item_id = 884, attr_type = ITEMATTR_VAL_FLEE, max_lv = 4, gear_point = 1, eff = 3, chance_combine = {1, 1, 0.8, 0.7, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.8, 0.7, 0, 0, 0, 0, 0}},
+ 
+	-- Spirit gem 
+	[8] = {item_id = 887, attr_type = ITEMATTR_VAL_STA, max_lv = 4, gear_point = 1, eff = 2, chance_combine = {1, 1, 0.8, 0.7, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.8, 0.7, 0, 0, 0, 0, 0}},
+ 
+	-- Gem of the wind 
+	[9] = {item_id = 860, attr_type = ITEMATTR_VAL_AGI, max_lv = 0, gear_point = 5, eff = 5, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Gem of striking 
+	[10] = {item_id = 861, attr_type = ITEMATTR_VAL_DEX, max_lv = 0, gear_point = 5, eff = 5, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Gem of colossus 
+	[11] = {item_id = 862, attr_type = ITEMATTR_VAL_CON, max_lv = 0, gear_point = 5, eff = 5, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Gem of rage 
+	[12] = {item_id = 863, attr_type = ITEMATTR_VAL_STR, max_lv = 0, gear_point = 5, eff = 5, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Eye of Black Dragon 
+	[13] = {item_id = 864, attr_type = ITEMATTR_VAL_MNATK, max_lv = 0, gear_point = 6, eff = 50, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Soul of Black Dragon 
+	[14] = {item_id = 865, attr_type = ITEMATTR_VAL_PDEF, max_lv = 0, gear_point = 10, eff = 3, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Heart of Black Dragon 
+	[15] = {item_id = 866, attr_type = ITEMATTR_VAL_MXHP, max_lv = 0, gear_point = 9, eff = 500, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Gem of soul 
+	[16] = {item_id = 1012, attr_type = ITEMATTR_VAL_STA, max_lv = 0, gear_point = 5, eff = 5, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+
+	-- Yellow jade 
+	[17] = {item_id = 5750, attr_type = ITEMATTR_VAL_DEF, max_lv = 3, gear_point = 3, eff = 15, chance_combine = {1, 1, 0.9, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.95, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Red jade 
+	[18] = {item_id = 5751, attr_type = ITEMATTR_VAL_MXHP, max_lv = 3, gear_point = 4, eff = 250, chance_combine = {1, 1, 0.9, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.95, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Green jade 
+	[19] = {item_id = 5752, attr_type = ITEMATTR_VAL_MXSP, max_lv = 3, gear_point = 4, eff = 200, chance_combine = {1, 1, 0.9, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.95, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Chiatana's Aura 
+	[20] = {item_id = 5771, attr_type = ITEMATTR_VAL_CRT, max_lv = 0, gear_point = 8, eff = 10, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Lock's Strike 
+	[21] = {item_id = 5772, attr_type = ITEMATTR_VAL_HIT, max_lv = 0, gear_point = 4, eff = 10, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Bing's Dodging 
+	[22] = {item_id = 5773, attr_type = ITEMATTR_VAL_FLEE, max_lv = 0, gear_point = 4, eff = 10, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Feng's Defense 
+	[23] = {item_id = 5774, attr_type = ITEMATTR_VAL_DEF, max_lv = 0, gear_point = 5, eff = 15, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Shark's Strengthening 
+	[24] = {item_id = 5775, attr_type = ITEMATTR_VAL_MXHP, max_lv = 0, gear_point = 5, eff = 300, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Azrael's Glare 
+	[25] = {item_id = 5845, attr_type = ITEMATTR_VAL_STR, max_lv = 0, gear_point = 10, eff = 8, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Undead Azrael 
+	[26] = {item_id = 5846, attr_type = ITEMATTR_VAL_CON, max_lv = 0, gear_point = 10, eff = 8, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Light of Azrael 
+	[27] = {item_id = 5847, attr_type = ITEMATTR_VAL_STA, max_lv = 0, gear_point = 10, eff = 8, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Azrael's Aggregation 
+	[28] = {item_id = 5848, attr_type = ITEMATTR_VAL_DEX, max_lv = 0, gear_point = 10, eff = 8, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Azrael's Dance 
+	[29] = {item_id = 5849, attr_type = ITEMATTR_VAL_AGI, max_lv = 0, gear_point = 10, eff = 8, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Broken gem of the wind 
+	[30] = {item_id = 6817, attr_type = ITEMATTR_VAL_AGI, max_lv = 3, gear_point = 2, eff = 2, chance_combine = {1, 1, 0.9, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.95, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Cracked gem of the wind 
+	[31] = {item_id = 6818, attr_type = ITEMATTR_VAL_AGI, max_lv = 4, gear_point = 3, eff = 3, chance_combine = {1, 1, 0.9, 0.8, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.95, 0.85, 0, 0, 0, 0, 0}},
+ 
+	-- Chipped gem of the wind 
+	[32] = {item_id = 6819, attr_type = ITEMATTR_VAL_AGI, max_lv = 3, gear_point = 4, eff = 4, chance_combine = {1, 1, 0.9, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.95, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Broken gem of striking 
+	[33] = {item_id = 6820, attr_type = ITEMATTR_VAL_DEX, max_lv = 3, gear_point = 2, eff = 2, chance_combine = {1, 1, 0.9, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.95, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Cracked gem of striking 
+	[34] = {item_id = 6821, attr_type = ITEMATTR_VAL_DEX, max_lv = 3, gear_point = 3, eff = 3, chance_combine = {1, 1, 0.9, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.95, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Chipped gem of striking 
+	[35] = {item_id = 6822, attr_type = ITEMATTR_VAL_DEX, max_lv = 2, gear_point = 4, eff = 4, chance_combine = {1, 1, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Broken gem of colossus 
+	[36] = {item_id = 6823, attr_type = ITEMATTR_VAL_CON, max_lv = 3, gear_point = 2, eff = 2, chance_combine = {1, 1, 0.9, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.95, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Cracked gem of colossus 
+	[37] = {item_id = 6824, attr_type = ITEMATTR_VAL_CON, max_lv = 3, gear_point = 3, eff = 3, chance_combine = {1, 1, 0.9, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.95, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Chipped gem of colossus 
+	[38] = {item_id = 6825, attr_type = ITEMATTR_VAL_CON, max_lv = 3, gear_point = 4, eff = 4, chance_combine = {1, 1, 0.9, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.95, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Broken gem of rage 
+	[39] = {item_id = 6826, attr_type = ITEMATTR_VAL_STR, max_lv = 3, gear_point = 2, eff = 2, chance_combine = {1, 1, 0.9, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.95, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Cracked gem of rage 
+	[40] = {item_id = 6827, attr_type = ITEMATTR_VAL_STR, max_lv = 3, gear_point = 3, eff = 3, chance_combine = {1, 1, 0.9, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.95, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Chipped gem of rage 
+	[41] =  {item_id = 6828, attr_type = ITEMATTR_VAL_STR, max_lv = 2, gear_point = 4, eff = 4, chance_combine = {1, 1, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Broken gem of soul 
+	[42] =  {item_id = 6829, attr_type = ITEMATTR_VAL_STA, max_lv = 3, gear_point = 2, eff = 2, chance_combine = {1, 1, 0.9, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.95, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Cracked gem of soul 
+	[43] =  {item_id = 6830, attr_type = ITEMATTR_VAL_STA, max_lv = 3, gear_point = 3, eff = 3, chance_combine = {1, 1, 0.9, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0.95, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Chipped gem of soul 
+	[44] =  {item_id = 6831, attr_type = ITEMATTR_VAL_STA, max_lv = 2, gear_point = 4, eff = 4, chance_combine = {1, 1, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {1, 1, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Great gem of rage 
+	[45] =  {item_id = 7108, attr_type = ITEMATTR_VAL_STR, max_lv = 0, gear_point = 6, eff = 6, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Great gem of soul 
+	[46] =  {item_id = 7109, attr_type = ITEMATTR_VAL_STA, max_lv = 0, gear_point = 6, eff = 6, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Great gem of striking 
+	[47] =  {item_id = 7110, attr_type = ITEMATTR_VAL_DEX, max_lv = 0, gear_point = 6, eff = 6, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Great gem of colossus 
+	[48] =  {item_id = 7111, attr_type = ITEMATTR_VAL_CON, max_lv = 0, gear_point = 6, eff = 6, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Great gem of wind 
+	[49] =  {item_id = 7112, attr_type = ITEMATTR_VAL_AGI, max_lv = 0, gear_point = 6, eff = 6, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Golden rock 
+	[50] =  {item_id = 7288, attr_type = ITEMATTR_VAL_STR, max_lv = 0, gear_point = 2, eff = 2, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Lumber rock 
+	[51] =  {item_id = 7289, attr_type = ITEMATTR_VAL_STA, max_lv = 0, gear_point = 2, eff = 2, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Water rock 
+	[52] =  {item_id = 7290, attr_type = ITEMATTR_VAL_DEX, max_lv = 0, gear_point = 2, eff = 2, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Fire rock 
+	[53] =  {item_id = 7291, attr_type = ITEMATTR_VAL_AGI, max_lv = 0, gear_point = 2, eff = 2, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Earth rock 
+	[54] =  {item_id = 7292, attr_type = ITEMATTR_VAL_CON, max_lv = 0, gear_point = 2, eff = 2, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Breath of the Black Dragon 
+	[55] =  {item_id = 1862, attr_type = ITEMATTR_VAL_MXSP, max_lv = 0, gear_point = 9, eff = 250, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Bright Black Dragon eye 
+	[56] =  {item_id = 242, attr_type = ITEMATTR_VAL_MNATK, max_lv = 0, gear_point = 3, eff = 20, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Faceted Fiery Gem 
+	[57] =  {item_id = 10303, attr_type = ITEMATTR_VAL_MNATK, max_lv = 0, gear_point = 2, eff = 8, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Faceted Furious Gem 
+	[58] =  {item_id = 10304, attr_type = ITEMATTR_VAL_MNATK, max_lv = 0, gear_point = 2, eff = 12, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Faceted Explosive Gem 
+	[59] =  {item_id = 10305, attr_type = ITEMATTR_VAL_MNATK, max_lv = 0, gear_point = 2, eff = 20, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Faceted Glowing Gem 
+	[60] =  {item_id = 10306, attr_type = ITEMATTR_VAL_DEF, max_lv = 0, gear_point = 2, eff = 10, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Faceted Shining Gem 
+	[61] =  {item_id = 10307, attr_type = ITEMATTR_VAL_MXHP, max_lv = 0, gear_point = 4, eff = 200, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Faceted Shadow Gem 
+	[62] =  {item_id = 10308, attr_type = ITEMATTR_VAL_FLEE, max_lv = 0, gear_point = 2, eff = 4, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Faceted Spirit Gem 
+	[63] =  {item_id = 10309, attr_type = ITEMATTR_VAL_STA, max_lv = 0, gear_point = 2, eff = 2, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Admiral-Gem of the wind 
+	[64] =  {item_id = 7447, attr_type = ITEMATTR_VAL_AGI, max_lv = 0, gear_point = 1, eff = 1, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Admiral-Gem of striking 
+	[65] =  {item_id = 7448, attr_type = ITEMATTR_VAL_DEX, max_lv = 0, gear_point = 1, eff = 1, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Admiral-Gem of colossus 
+	[66] =  {item_id = 7449, attr_type = ITEMATTR_VAL_CON, max_lv = 0, gear_point = 1, eff = 1, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Admiral-Gem of rage 
+	[67] =  {item_id = 7450, attr_type = ITEMATTR_VAL_STR, max_lv = 0, gear_point = 1, eff = 1, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+ 
+	-- Admiral-Gem of soul 
+	[68] =  {item_id = 7451, attr_type = ITEMATTR_VAL_STA, max_lv = 0, gear_point = 1, eff = 1, chance_combine = {0, 0, 0, 0, 0, 0, 0, 0, 0}, chance_forge = {0, 0, 0, 0, 0, 0, 0, 0, 0}}
+}
+------
+-- Узнаем максимальный уровень самоцвета по идентификатору
+------
+function GetStoneMaxLv(item_id)
+	local max_level = 4
+	
+	for pos, value in pairs(STONE) do
+		if (value.item_id == item_id) then
+			max_level = value.max_lv
+			break
+		end
+	end
+	
+	return max_level
+end
+
+------
+-- Конвертируем описание в типы предметов, в которые можно ковать необходимый самоцвет
+------
+function getStoneItemType(stone_id)
+	-- Узнаем описание предмета
+	local get_item_description = GetItemDescription(stone_id)
+	-- Если итем изначально кривой без описания
+	if (get_item_description == '0') then
+		-- Завершаем сценарий
+		return false
+	end
+	
+	-- Объявляем таблицу
+	local tables = {}
+	-- Парсим описание, через запятую должны находиться типы эквипа
+	local _exp = explode(',', get_item_description)
+	
+	-- Циклом идем по массиву (_exp)
+	for _, value in pairs(_exp) do
+		-- На всякий случай преобразуем в число
+		value = tonumber(value)
+		-- Добавляем наш тип предмета в массив
+		table.insert(tables, value)
+	end
+	
+	-- Добавим к нашей таблице нолик в конце, по дефолту везде нолик юзается
+	table.insert(tables, 0)
+	
+	-- Сценарий завершается возвращением таблицы
+	return tables
+end
+
+------
+-- Возвращает идентификатор-позицию самоцвета в таблице @STONE
+-- @see ItemUse_Rent65()
+------
+function stonePos(stone_id)
+	local pos = 0
+	for i, value in pairs(STONE) do
+		if (value.item_id == stone_id) then
+			pos = i
+			break
+		end
+	end
+	
+	return pos
+end
